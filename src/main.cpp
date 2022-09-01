3d51a51f5d82348b3fb6682ebc0b0c22cab91feb
MR2K5 <massimo.ruohonen@outlook.com>
2022-09-01 15:41:08 +0300

    Return with error codes on exception

--- src/main.cpp
+++ src/main.cpp
@@ -53,39 +53,50 @@ private:
     std::shared_ptr<sys_info::SystemInfo> sysinfo;
 };
 
-std::atomic_flag stop_all = ATOMIC_FLAG_INIT;
+std::atomic_flag stop_all           = ATOMIC_FLAG_INIT;
+std::atomic_bool stopped_with_error = false;
 
 void stop_handler(int) {
     stop_all.clear();
 }
 
 int main() {
-    Ruuvitag rv;
-    stop_all.test_and_set();
+    try {
+        Ruuvitag rv;
+        stop_all.test_and_set();
 
-    std::thread runner([&rv]() {
-        try {
-            rv.start();
-        } catch (std::exception const& e) {
-            // Stop
-            stop_all.clear();
-            std::cerr << "Runner thread exited with error: " << e.what() << "\n";
-        }
-    });
+        std::thread runner([&rv]() {
+            try {
+                rv.start();
+            } catch (std::exception const& e) {
+                // Stop
+                stop_all.clear();
+                stopped_with_error = true;
+                std::cerr << "Runner thread exited with error: " << e.what() << "\n";
+            }
+        });
 
-    std::thread stopper([&rv]() {
-        while (stop_all.test_and_set()) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        }
-        log("Stopping...");
-        rv.stop();
-    });
+        std::thread stopper([&rv]() {
+            while (stop_all.test_and_set()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            }
+            log("Stopping...");
+            rv.stop();
+        });
 
-    std::signal(SIGTERM, stop_handler);
-    std::signal(SIGINT, stop_handler);
+        std::signal(SIGTERM, stop_handler);
+        std::signal(SIGINT, stop_handler);
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    stopper.join();
-    runner.join();
+        stopper.join();
+        runner.join();
+    } catch (std::exception const& e) {
+        logging::log("Uncaught exception: ", e.what());
+        stopped_with_error = true;
+    } catch (...) {
+        logging::log("Uncaught exception of unknown type in main()");
+        stopped_with_error = true;
+    }
+    return stopped_with_error ? EXIT_FAILURE : EXIT_SUCCESS;
 }
