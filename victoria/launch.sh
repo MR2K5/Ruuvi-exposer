9c12bfe7a9e9505504766167e11aa758dbbd45d5
MR2K5 <massimo.ruohonen@outlook.com>
2023-03-16 18:33:14 +0200

    set victoriametrics flush interval to save writes

--- victoria/launch.sh
+++ victoria/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./victoria-metrics-prod -retentionPeriod=100y -promscrape.config=/etc/victoria-scrape-config.yml
+./victoria-metrics-prod -retentionPeriod=100y -promscrape.config=/etc/victoria-scrape-config.yml -inmemoryDataFlushInterval=5m
 
